2a3a847c80ba4b27262d2fba1a648492d17f9216
jumaan1st <mohammedjumaan.mf@gmail.com>
2025-08-21 18:08:40 +0530

    done

--- server/app.py
+++ server/app.py
@@ -38,7 +38,7 @@ app.config.update(
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
     JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY'),
     JWT_TOKEN_LOCATION=['cookies'],
-    JWT_COOKIE_SECURE=False,  # Set to True in production
+    JWT_COOKIE_SECURE=False,  
     JWT_COOKIE_SAMESITE='Lax',
     JWT_ACCESS_COOKIE_NAME='access_token',
     JWT_COOKIE_CSRF_PROTECT=False
